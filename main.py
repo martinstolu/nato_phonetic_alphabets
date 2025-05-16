ed1cc509cad27394e182ecc9e2dc6b62041b55fe
P45c4l-cyber <tolu.martins.a@gmail.com>
2025-05-16 18:29:56 +0200

    updating code

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas
 
-user_input= input("Write a word: ").upper()
 nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
-
 new_dic = {row.letter:row.code for (index, row) in nato_data.iterrows()}
-word_to_print =[new_dic[letter] for letter in user_input]
-print(word_to_print)
+
+user_input_digits = True
+while user_input_digits:
+
+    user_input= input("Enter a word: ").upper()
+    try:
+        word_to_print =[new_dic[letter] for letter in user_input]
+    except KeyError:
+        print("sorry, only letters in the alphabet please.")
+    else:
+        print(word_to_print)
+        user_input_digits = False
